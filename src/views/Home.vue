c02c5ee3a7bb78859c7a778d6075218a3a2fb7d3
objectMaker <528154320@qq.com>
2022-04-10 23:03:19 +0800

    '项目清理完成'

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,9 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
-  </div>
+  <div class="home">我是home哟</div>
+  <div>{{ homeWords }}</div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from "vue";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
-
-export default defineComponent({
-  name: "Home",
-  components: {
-    HelloWorld,
-  },
-});
+<script lang="ts" setup>
+import { ref } from "vue";
+const homeWords = ref("homeWords");
 </script>
