2871db523724f5ce6405625f9b50f5e7d0b65ce7
objectMaker <528154320@qq.com>
2022-09-27 01:55:07 +0800

    'animate.css使用'

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import App from "./App.vue";
 import router from "./router";
 import "normalize.css"; //全局引入重置样式表
 // import "@/mock/index.ts";
+import animate from "animate.css";
 import "./index.css";
 import { createPinia } from "pinia";
 import "./Plugins/testDecorator";
@@ -10,4 +11,9 @@ import "./Plugins/testDecorator";
 import ElementPlus from "element-plus";
 import "element-plus/dist/index.css";
 const pinia = createPinia();
-createApp(App).use(router).use(ElementPlus).use(pinia).mount("#app");
+createApp(App)
+  .use(router)
+  .use(ElementPlus)
+  .use(animate)
+  .use(pinia)
+  .mount("#app");
--- src/views/Abc/index.vue
+++ src/views/Abc/index.vue
@@ -1,5 +1,6 @@
 <template>
-  <div></div>
+  <h1 class="animate_animated animate_bounce">Example</h1>
+  <h1 class="animate__animated animate__bounce animate__infinite">{{ 123 }}</h1>
 </template>
 <script setup lang="ts"></script>
 <style scoped lang="scss"></style>
